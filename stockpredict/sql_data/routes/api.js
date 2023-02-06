188539917e0ad7d335d5ee54d6c2349f057ffdc3
andy <andy940047@gmail.com>
2023-02-06 11:35:19 +0800

    2_6

---
+++ stockpredict/sql_data/routes/api.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+const data = [{ id: 1, name: "小明" }];
+// 設置請求路徑為 /products 請求方法為 get
+router.get("/", function (req, res, next) {
+  res.send({ success: true, data }).end();
+});
+
+module.exports = router;
